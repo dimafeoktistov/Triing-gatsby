03fd871fd3be47ec89bc18f476b3ace765190959
Dmitriy Feoktistov <feoktistovdmitriy@gmail.com>
2018-06-14 21:56:10 +0700

    FEAT: Add deYoung theme for typography

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,5 +1,6 @@
 import Typography from 'typography'
+import deYoungTheme from 'typography-theme-de-young'
 
-const typography = new Typography({ baseFontSize: '18px' })
+const typography = new Typography(deYoungTheme)
 
 export default typography
