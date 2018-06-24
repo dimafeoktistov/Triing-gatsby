2e9f0d46418bdc2a21ed58dfed3c0ff59c6c7ca1
Dmitriy Feoktistov <feoktistovdmitriy@gmail.com>
2018-06-24 22:10:59 +0700

    fix: Changed path to header in layout component

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
-import Header from '../components/Header/header'
+import Header from '../containers/Header/header'
 import Footer from '../components/Footer/Footer'
 import Aux from '../HOCS/Aux.js'
 
