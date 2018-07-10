20875b94abc2e498019b7511c63cf9f6393b849b
Dima Feoktistov <feoktistovdmitriy@gmail.com>
2018-07-10 22:17:11 +0700

    Fix: Did equal width for header and container

--- src/components/container.js
+++ src/components/container.js
@@ -1,14 +1,6 @@
 import React from 'react'
+import styles from './container.module.scss'
 
 export default ({ children }) => (
-  <div
-    style={{
-      margin: '0 auto',
-      maxWidth: 960,
-      padding: '0px 1.0875rem 1.45rem',
-      paddingTop: 0,
-    }}
-  >
-    {children}
-  </div>
+  <div className={styles.container}>{children}</div>
 )
